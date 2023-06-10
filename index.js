94d9551e74b4c30654251ad16fed1f29ba107c00
yoonlang <cdt9473@gmail.com>
2023-06-10 15:38:25 +0900

    기본적인 list 생성 완료

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const chattingList = document.querySelector(
   ".chat-scrollable-area__message-container"
 );
 
+const handledChattingList = [];
+
 const observer = new MutationObserver((e) => {
   e.forEach((mutationRecord) => {
     if (mutationRecord.addedNodes.length === 0) return;
-    console.log(
-      mutationRecord.addedNodes[0].querySelector(".chat-author__display-name")
-        .innerHTML,
-      mutationRecord.addedNodes[0].querySelector(".chat-author__intl-login")
-        .innerHTML,
-      mutationRecord.addedNodes[0].querySelector(".text-fragment").innerHTML
-    );
+    const userName = mutationRecord.addedNodes[0].querySelector(
+      ".chat-author__display-name"
+    ).innerHTML;
+    const userId = mutationRecord.addedNodes[0]
+      .querySelector(".chat-author__intl-login")
+      ?.innerHTML.replace(/\(|\)/g, "");
+    let chatting = "";
+    mutationRecord.addedNodes[0]
+      .querySelector(`span[data-a-target="chat-line-message-body"]`)
+      .childNodes.forEach((node) => {
+        if (node.tagName === "DIV") {
+          chatting += `(이모티콘)`;
+        } else {
+          chatting += node.innerHTML;
+        }
+      });
+    const chattingData = {
+      userName,
+      userId: userId ?? userName,
+      chatting,
+    };
+    handledChattingList.push(chattingData);
   });
 });
 
